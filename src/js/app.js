1fe0cfdb20e1835caccddd87d271b9ce0ab8d951
Dries Standaert <dries.standaert@AT00399DS.local>
2021-04-11 14:10:39 +0100

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,153 @@
+import '../scss/app.scss';
+
+
+////////////////////
+// Count count count
+////////////////////
+
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  let dday = "Apr 16, 2021 16:30:00",
+      countDown = new Date(dday).getTime(),
+      x = setInterval(function() {    
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          let headline = document.getElementById("headline"),
+              countdown = document.getElementById("countdown");
+
+          headline.innerText = "BOOM";
+          countdown.style.display = "none";
+
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
+
+
+////////////////////
+// paint paint paint
+////////////////////
+
+let canvas = document.querySelector('#draw');
+        let ctx = canvas.getContext('2d');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        ctx.strokeStyle = '#F75F2A';
+        ctx.lineJoin = 'round';
+        ctx.lineCap = 'round';
+        ctx.lineWidth = 200;
+        let isDrawing = false;
+        let lastX = 0;
+        let lastY = 0;
+        let hue = 0;
+        let direction = true;
+
+        function draw(e) {
+          isDrawing = true;
+            if (!isDrawing) return;
+            // ctx.strokeStyle = `hsl(${hue},100%,50%)`;
+            ctx.strokeStyle = '#FF9966';
+            ctx.beginPath();
+            ctx.moveTo(lastX, lastY);
+            ctx.lineTo(e.offsetX, e.offsetY);
+            ctx.stroke();
+            [lastX, lastY] = [e.offsetX, e.offsetY];
+            hue++;
+            if (hue >= 10) { //360
+                hue = 0;
+            };
+
+            if (ctx.lineWidth >= 10 || ctx.lineWidth <= 1) { //100
+                direction = !direction;
+            };
+
+            if (direction) {
+                ctx.lineWidth++;
+            } else {
+                ctx.lineWidth--;
+            };
+
+        };
+
+        // canvas.addEventListener('mousedown', (e) => {
+        //     isDrawing = true;
+        //     [lastX, lastY] = [e.offsetX, e.offsetY];
+        // });
+        canvas.addEventListener('mousemove', draw);
+
+        canvas.addEventListener('mouseup', () => isDrawing = false);
+        canvas.addEventListener('mouseout', () => isDrawing = false);
+
+
+
+//////////////////
+// drag drag drag
+//////////////////
+
+$( ".js-drag" ).draggable({
+  containment: ".js-dragzone"
+});
+
+
+// Ensure that full animation plays.
+// $(".js-key").bind("webkitAnimationEnd mozAnimationEnd oanimationEnd animationEnd", function() {
+//   $(this).removeClass("js-animate");
+// })
+
+// Stop and start sound
+function playSound() {
+    var sound = document.getElementById('js-sound');
+    if (sound.paused) {
+        sound.play();
+    } else{
+        sound.currentTime = 0
+    }
+}
+
+// clone elements
+function cloneShizzle() {
+  var $div = $('div[id^="clone"]:last');
+      // get viewport width/height
+      const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+      const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
+      // Read the Number from that DIV's ID
+      // And increment that number by 1
+      var num = parseInt( $div.prop("id").match(/\d+/g), 10 ) +1;
+
+      // Clone it and assign the new ID 
+      var $clone = $div.clone().prop('id', 'clone'+num ).css({"left": Math.floor(Math.random() * vw) + "px", "top": Math.floor(Math.random() * vh) + "px"});
+
+      // Insert $clone
+      $clone.appendTo('body').draggable();
+}
+
+
+
+// initiate animation/sound by keystroke
+$(window).keydown(function(event) {
+  event.preventDefault();
+  switch (event.which) {
+    // key Spacebar
+    case 32:
+      playSound();
+      cloneShizzle();
+      break;
+    default:
+      break;
+  }
+})
---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const serverConfiguration = {
+  internal: {
+    server: {
+      baseDir: 'dist',
+    },
+    port: 3000,
+  },
+  external: {
+    proxy: 'http://localhost:9000/path/to/project/',
+  },
+};
+
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+let targetServerConfiguration = serverConfiguration.internal;
+
+const config = function(env, args) {
+  if (args.externalServer !== undefined && args.externalServer) {
+    targetServerConfiguration = serverConfiguration.external;
+  }
+
+  return {
+    entry: {
+      app: './src/js/app.js',
+    },
+    output: {
+      filename: 'js/[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: { name: 'fonts/[name].[hash:6].[ext]', publicPath: '../', limit: 8192 },
+            },
+          ],
+        },
+      ],
+    },
+    watchOptions: {
+      poll: 1000,
+      ignored: /node_modules/,
+    },
+    plugins: [
+      new BrowserSyncPlugin({
+        ...targetServerConfiguration,
+        files: ['src/*'],
+        ghostMode: {
+          clicks: false,
+          location: false,
+          forms: false,
+          scroll: false,
+        },
+        injectChanges: true,
+        logFileChanges: true,
+        logLevel: 'debug',
+        logPrefix: 'webpack',
+        notify: true,
+        reloadDelay: 0,
+      }),
+      new HtmlWebpackPlugin({
+        inject: true,
+        hash: false,
+        filename: 'index.html',
+        template: path.resolve(__dirname, 'src', 'index.html'),
+        favicon: path.resolve(__dirname, 'src', 'assets/images', 'favicon.png'),
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src', 'assets'),
+            to: path.resolve(__dirname, 'dist', 'assets'),
+            toType: 'dir',
+          },
+          {
+            from: path.resolve(__dirname, 'src', 'fonts'),
+            to: path.resolve(__dirname, 'dist', 'fonts'),
+            toType: 'dir',
+          },
+        ],
+      }),
+    ],
+  };
+};
+
+module.exports = config;
